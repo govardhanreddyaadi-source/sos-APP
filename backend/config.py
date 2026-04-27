5193161c950428ec3144c1ffb11698e019026c92
govardhanreddyaadi-source <govardhanreddyaadi@gmail.com>
2026-04-28 00:16:22 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb+srv://admin:1234@sosemergency.iy3mloj.mongodb.net/?appName=sosemergency")
 db = client["sos_emergency"]
